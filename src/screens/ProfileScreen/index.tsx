f7c6ccfdd9919463bb908ce2ba29853691c64c09
Yusuf Cagan <yusuf@diffea.com>
2025-10-19 03:15:52 +0300

    feat: profile screen

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,10 +1,77 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from './styles';
+import PenIcon from '../../assets/icon/pen-icon';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getFriend } from '../../api/friend';
+import TrashIcon from '../../assets/icon/trash-icon';
 
 export default function ProfileScreen() {
+  const { data: getFriendData } = useQuery({
+    queryKey: ['getFriendData'],
+    queryFn: getFriend,
+  });
+  const getTimeAgo = (createdAt: string) => {
+    const now = new Date();
+    const created = new Date(createdAt);
+    const diffMs = now.getTime() - created.getTime();
+
+    const minutes = Math.floor(diffMs / (1000 * 60));
+    const hours = Math.floor(diffMs / (1000 * 60 * 60));
+    const days = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+    if (minutes < 1) return 'just now';
+    if (minutes < 60) return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+    if (hours < 24) return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    return `${days} day${days > 1 ? 's' : ''} ago`;
+  };
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView style={styles.flex} edges={['top']}>
+      <View style={styles.header}>
+        <View style={styles.row}>
+          <Text style={styles.title}>Profile</Text>
+          <View style={styles.settings}>
+            <PenIcon width={25} />
+          </View>
+        </View>
+        <View style={styles.userContainer}>
+          <View style={styles.userIcon} />
+          <View style={{ marginLeft: 10 }}>
+            <Text style={styles.username}>@{'username'}</Text>
+            <View style={styles.mailContainer}>
+              <Text style={styles.mailText}>mail@mail.com</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.line} />
+      <View style={styles.line} />
+      <View style={styles.content}>
+        <FlatList
+          data={getFriendData?.data}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.userCard}>
+              <View style={styles.flexRow}>
+                <Image
+                  source={{ uri: item.profilePic2 }}
+                  style={styles.userImage}
+                />
+                <View style={styles.userTextContainer}>
+                  <Text style={styles.userCardUsername}>@{item.username1}</Text>
+                  <Text style={styles.lastSeen}>
+                    Last Seen: {getTimeAgo(item.updatedAt)}
+                  </Text>
+                </View>
+              </View>
+              <TouchableOpacity style={styles.settings}>
+                <TrashIcon width={25} />
+              </TouchableOpacity>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ src/screens/ProfileScreen/styles.ts
@@ -0,0 +1,101 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../../assets/theme/Colors';
+
+export const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+    backgroundColor: Colors.White,
+  },
+  header: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    color: Colors.Black,
+  },
+  settings: {
+    width: 45,
+    height: 45,
+    borderRadius: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#0000001A',
+  },
+  userContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  userIcon: {
+    height: 70,
+    width: 70,
+    borderRadius: 40,
+    borderWidth: 1,
+    borderColor: '#0000001A',
+  },
+  username: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: Colors.Black,
+  },
+  mailContainer: {
+    backgroundColor: '#E8DAFF',
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  mailText: {
+    color: Colors.Primary,
+    fontSize: 16,
+    fontWeight: '500',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  line: {
+    borderTopWidth: 1,
+    borderColor: '#0000001A',
+    marginTop: 15,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#F3F6FD',
+    padding: 20,
+  },
+  flexRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userCard: {
+    backgroundColor: Colors.White,
+    borderRadius: 10,
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    justifyContent: 'space-between',
+  },
+  userImage: {
+    width: 25,
+    height: 25,
+    borderRadius: 15,
+  },
+  userTextContainer: {
+    marginLeft: 10,
+  },
+  userCardUsername: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: Colors.Black,
+  },
+  lastSeen: {
+    fontSize: 13,
+    fontWeight: '500',
+    color: Colors.Gray,
+  },
+});
